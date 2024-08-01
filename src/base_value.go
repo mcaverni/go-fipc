82c6de24bbd6f50a9853c7d277381070c2bc5de2
Massimo Caverni <m.caverni@frigel.com>
2024-08-01 17:20:34 +0200

    Implemented binary handling of values

--- src/base_value.go
+++ src/base_value.go
@@ -1,5 +1,10 @@
 package gofipc
 
+import (
+	"fmt"
+	"math"
+)
+
 type Value interface {
 	MarshalBinary() ([]byte, error)
 	UnmarshalBinary([]byte) error
@@ -9,13 +14,66 @@ type ValueInt int32
 type ValueFloat float32
 type ValueString string
 type ValueBool bool
-type ValueChar rune
+type ValueChar byte
 
 func (v ValueInt) MarshalBinary() ([]byte, error) {
 	return []byte{byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)}, nil
 }
 
 func (v *ValueInt) UnmarshalBinary(b []byte) error {
+	if len(b) != 4 {
+		return fmt.Errorf("invalid buffer length")
+	}
 	*v = ValueInt(int32(b[0])<<24 | int32(b[1])<<16 | int32(b[2])<<8 | int32(b[3]))
+	return nil
+}
+
+func (v ValueFloat) MarshalBinary() ([]byte, error) {
+	i := math.Float32bits(float32(v))
+	return []byte{byte(i >> 24), byte(i >> 16), byte(i >> 8), byte(i)}, nil
+}
+
+func (v *ValueFloat) UnmarshalBinary(b []byte) error {
+	if len(b) != 4 {
+		return fmt.Errorf("invalid buffer length")
+	}
+	i := uint32(b[0])<<24 | uint32(b[1])<<16 | uint32(b[2])<<8 | uint32(b[3])
+	*v = ValueFloat(math.Float32frombits(i))
+	return nil
+}
+
+func (v ValueString) MarshalBinary() ([]byte, error) {
+	return []byte(v), nil
+}
+
+func (v *ValueString) UnmarshalBinary(b []byte) error {
+	*v = ValueString(b)
+	return nil
+}
+
+func (v ValueBool) MarshalBinary() ([]byte, error) {
+	if v {
+		return []byte{1}, nil
+	}
+	return []byte{0}, nil
+}
+
+func (v *ValueBool) UnmarshalBinary(b []byte) error {
+	if len(b) != 1 {
+		return fmt.Errorf("invalid buffer length")
+	}
+	*v = b[0] != 0
+	return nil
+}
+
+func (v ValueChar) MarshalBinary() ([]byte, error) {
+	return []byte{byte(v)}, nil
+}
+
+func (v *ValueChar) UnmarshalBinary(b []byte) error {
+	if len(b) != 1 {
+		return fmt.Errorf("invalid buffer length")
+	}
+	*v = ValueChar(byte(b[0]))
 	return nil
 }
--- src/base_value_test.go
+++ src/base_value_test.go
@@ -1,1 +1,104 @@
 package gofipc
+
+import (
+	"testing"
+)
+
+func TestInteger(t *testing.T) {
+	v := ValueInt(0x12345678)
+	buf, err := v.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(buf) != 4 {
+		t.Errorf("Expected 4 bytes, got %d", len(buf))
+	}
+
+	var v2 ValueInt
+	err = v2.UnmarshalBinary(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	if v != v2 {
+		t.Errorf("Expected %d, got %d", v, v2)
+	}
+}
+
+func TestFloat(t *testing.T) {
+	v := ValueFloat(3.14159)
+	buf, err := v.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(buf) != 4 {
+		t.Errorf("Expected 4 bytes, got %d", len(buf))
+	}
+
+	var v2 ValueFloat
+	err = v2.UnmarshalBinary(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	if v != v2 {
+		t.Errorf("Expected %f, got %f", v, v2)
+	}
+}
+
+func TestString(t *testing.T) {
+	v := ValueString("Hello, World!")
+	buf, err := v.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(buf) != len(v) {
+		t.Errorf("Expected %d bytes, got %d", len(v), len(buf))
+	}
+	var v2 ValueString
+	err = v2.UnmarshalBinary(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	if v != v2 {
+		t.Errorf("Expected %s, got %s", v, v2)
+	}
+}
+
+func TestBool(t *testing.T) {
+	v := ValueBool(true)
+	buf, err := v.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(buf) != 1 {
+		t.Errorf("Expected 1 byte, got %d", len(buf))
+	}
+
+	var v2 ValueBool
+	err = v2.UnmarshalBinary(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	if v != v2 {
+		t.Errorf("Expected %t, got %t", v, v2)
+	}
+}
+
+func TestChar(t *testing.T) {
+	v := ValueChar('A')
+	buf, err := v.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(buf) != 1 {
+		t.Errorf("Expected 4 bytes, got %d", len(buf))
+	}
+
+	var v2 ValueChar
+	err = v2.UnmarshalBinary(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	if v != v2 {
+		t.Errorf("Expected %c, got %c", v, v2)
+	}
+}
